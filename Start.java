7823395a5311e0a8c118389f149294b96c0e34c5
Vitul Gupta <guptavitul@gmail.com>
2024-04-09 15:00:21 +0530

    Added Start file to my project

---
+++ Start.java
@@ -0,0 +1,12 @@
+public class Start {
+
+    public static void doSomething() {
+        System.out.println("Hiding");
+    }
+
+
+    public static void main(String[] args) {
+        doSomething();
+    }
+    
+}
