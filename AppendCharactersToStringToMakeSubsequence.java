825084a2eb511b8e9e8eb56737e44aa47df62795
Vitul Gupta <guptavitul@gmail.com>
2024-06-03 06:58:10 +0530

    leetcode 3rd june daily question solution

---
+++ AppendCharactersToStringToMakeSubsequence.java
@@ -0,0 +1,24 @@
+public class AppendCharactersToStringToMakeSubsequence {
+
+    public static int appendCharacter(String s, String t) {
+        int len = s.length();
+
+        int tlen = t.length();
+        int tidx = 0;
+
+        for(int i=0; i<len && tidx < t.length(); i++) {
+            char ch = s.charAt(i);
+            char tch = t.charAt(tidx);
+
+            if(ch == tch) {
+                tidx++;
+            }
+        }
+        return tlen-tidx;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(appendCharacter("z", "abcde"));
+    }
+}
