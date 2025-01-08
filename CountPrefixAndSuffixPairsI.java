7b54e75ca9963df9a3abee70ec22137bdff921c5
Vitul Gupta <guptavitul@gmail.com>
2025-01-08 14:02:11 +0530

    leetcode 8th Jan 2025 daily question solution

---
+++ CountPrefixAndSuffixPairsI.java
@@ -0,0 +1,23 @@
+public class CountPrefixAndSuffixPairsI {
+
+    public static boolean isPrefixAndSuffix(String str1, String str2) {
+        return str2.startsWith(str1) && str2.endsWith(str1);
+    }
+
+    public static int countPrefixSuffixPairs(String[] words) {
+        int pairs = 0;
+
+        for(int i=0; i<words.length; i++) {
+            for(int j=i+1; j<words.length; j++) {
+                if(isPrefixAndSuffix(words[i], words[j])) {
+                    pairs++;
+                }
+            }
+        }
+        return pairs;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countPrefixSuffixPairs(new String[]{"abab","ab"}));
+    }
+}
