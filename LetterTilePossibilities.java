565f4399337f0b89f58ad4d397ad9235aa204ada
Vitul Gupta <guptavitul@gmail.com>
2025-02-24 23:09:49 +0530

    leetcode 17th Feb 2025 daily question solution

--- LetterTilePossibilities.java
+++ LetterTilePossibilities.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class LetterTilePossibilities {
+
+    public static void generateLetterTile(String tiles, StringBuilder currString, Set<String> tilesSet, boolean[] isUsed) {
+        tilesSet.add(currString.toString());
+
+        for(int i=0; i<tiles.length(); i++) {
+            if(!isUsed[i]) {
+                isUsed[i] = true;
+                currString.append(tiles.charAt(i));
+                generateLetterTile(tiles, currString, tilesSet, isUsed);
+
+                currString.deleteCharAt(currString.length()-1);
+                isUsed[i] = false;
+            }
+        }
+
+    }
+
+    public static int numTilePossibilities(String tiles) {
+        Set<String> tilesSet = new HashSet<>();
+        boolean[] isUsed = new boolean[tiles.length()];
+
+        generateLetterTile(tiles, new StringBuilder(), tilesSet, isUsed);
+
+        return tilesSet.size()-1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numTilePossibilities("AAB"));
+    }
 }
