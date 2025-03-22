2ae2f33cbbb09119c5a9a6f5d05cc69a1fa85c32
Vitul Gupta <guptavitul@gmail.com>
2025-03-23 01:28:57 +0530

    implemeted Two Sum IV Input is BST using Inorder Traversal

--- Data_Structure_Algorithms_Topic_Wise_Questions/two_pointers/TwoSumIVInputIsABST.java
+++ Data_Structure_Algorithms_Topic_Wise_Questions/two_pointers/TwoSumIVInputIsABST.java
@@ -1,5 +1,8 @@
 package Data_Structure_Algorithms_Topic_Wise_Questions.two_pointers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class TreeNode {
     int val;
     TreeNode left;
@@ -12,14 +15,47 @@ class TreeNode {
 
 public class TwoSumIVInputIsABST {
 
-    public static boolean findTarget(TreeNode root) {
+    public static void inorderBSTTraversal(TreeNode node, List<Integer> list) {
+        if(node == null) {
+            return;
+        }
+
+        inorderBSTTraversal(node.left, list);
+        list.add(node.val);
+        inorderBSTTraversal(node.right, list);
+    }
+
+    public static boolean findTarget(TreeNode root, int k) {
         if(root == null) {
             return false;
+        }
+        List<Integer> numbers = new ArrayList<>();
+        inorderBSTTraversal(root, numbers);
+
+        int le = 0;
+        int ri = numbers.size()-1;
+
+        while(le < ri) {
+            if(numbers.get(le) + numbers.get(ri) == k) {
+                return true;
+            }else if(numbers.get(le) + numbers.get(ri) < k) {
+                le++;
+            }else {
+                ri--;
+            }
         }
         return false;
     }
 
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(findTarget(root, 19));
 
     }
 }
