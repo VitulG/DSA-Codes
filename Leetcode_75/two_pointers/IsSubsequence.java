be5c938610d87ebfe1d177f87403cbafe9b43187
Vitul Gupta <guptavitul@gmail.com>
2025-03-23 00:40:45 +0530

    implemented Is Subsequence problem

---
+++ Leetcode_75/two_pointers/IsSubsequence.java
@@ -0,0 +1,33 @@
+package Leetcode_75.two_pointers;
+
+public class IsSubsequence {
+
+    public static boolean isSubsequences(String s, String t) {
+        if(s.isEmpty()) {
+            return true;
+        }
+
+        if(t.isEmpty()) {
+            return false;
+        }
+
+        int sp = 0;
+        int tp = 0;
+
+        while(tp < t.length()) {
+            if(s.charAt(sp) == t.charAt(tp)) {
+                sp++;
+
+                if(sp == s.length()) {
+                    return true;
+                }
+            }
+            tp++;
+        }
+        return sp == s.length();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isSubsequences("b", "abc"));
+    }
+}
