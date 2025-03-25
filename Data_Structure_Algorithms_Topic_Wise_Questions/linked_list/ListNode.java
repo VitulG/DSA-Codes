859ae70c630cde82effc6a0b45824f364f4aed1f
Vitul Gupta <guptavitul@gmail.com>
2025-03-25 17:49:00 +0530

    Linked List structure class

---
+++ Data_Structure_Algorithms_Topic_Wise_Questions/linked_list/ListNode.java
@@ -0,0 +1,10 @@
+package Data_Structure_Algorithms_Topic_Wise_Questions.linked_list;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
