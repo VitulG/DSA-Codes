9454379bb7ad5acffbe93c0481d48a52c50cdd36
Vitul Gupta <guptavitul@gmail.com>
2025-03-26 18:36:14 +0530

    Overridden toString method to avoid printing the value in the questions again and again.

--- Data_Structure_Algorithms_Topic_Wise_Questions/linked_list/ListNode.java
+++ Data_Structure_Algorithms_Topic_Wise_Questions/linked_list/ListNode.java
@@ -7,4 +7,22 @@ public class ListNode {
     public ListNode(int val) {
         this.val = val;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        ListNode curr = this;  // Start from current node
+        while (curr != null) {
+            sb.append(curr.val);
+            if (curr.next != null) {
+                sb.append(", ");
+            }
+            curr = curr.next;
+        }
+
+        sb.append("]");
+        return sb.toString(); // Return the final string representation
+    }
 }
