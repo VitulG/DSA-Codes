2e35032219bc9d29623991de55a11af5a3c38010
Vitul Gupta <guptavitul@gmail.com>
2025-04-04 17:31:12 +0530

    created a class for a tree structure

---
+++ Data_Structure_Algorithms_Topic_Wise_Questions/trees/TreeNode.java
@@ -0,0 +1,11 @@
+package Data_Structure_Algorithms_Topic_Wise_Questions.trees;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
