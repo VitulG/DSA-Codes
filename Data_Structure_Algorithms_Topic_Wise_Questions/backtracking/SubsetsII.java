29bff5a4acfdc0cfc92a14d27c26bd9241597fbd
Vitul Gupta <guptavitul@gmail.com>
2025-04-16 17:07:05 +0530

    remove unused imports

--- Data_Structure_Algorithms_Topic_Wise_Questions/backtracking/SubsetsII.java
+++ Data_Structure_Algorithms_Topic_Wise_Questions/backtracking/SubsetsII.java
@@ -2,7 +2,6 @@ package Data_Structure_Algorithms_Topic_Wise_Questions.backtracking;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class SubsetsII {
