72c367215d8bc5bb0abca123966d029f95a6a3ae
Vitul Gupta <guptavitul@gmail.com>
2025-04-19 02:00:02 +0530

    make them default so that i can access these instance variables

--- Data_Structure_Algorithms_Topic_Wise_Questions/trie/TrieBitNode.java
+++ Data_Structure_Algorithms_Topic_Wise_Questions/trie/TrieBitNode.java
@@ -1,8 +1,8 @@
 package Data_Structure_Algorithms_Topic_Wise_Questions.trie;
 
 public class TrieBitNode {
-    private int bit;
-    private TrieBitNode[] children;
+    int bit;
+    TrieBitNode[] children;
 
     public TrieBitNode(int bit) {
         this.bit = bit;
