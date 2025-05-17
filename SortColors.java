f3827187a820feea40e3ae1d3ce385a50ec84f02
Vitul Gupta <guptavitul@gmail.com>
2025-05-17 09:54:14 +0530

    Implemented Sort colors using Dutch national flag algorithm

--- SortColors.java
+++ SortColors.java
@@ -18,6 +18,36 @@ public class SortColors {
 
     }
 
+    public static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+
+    public static void sortColorsUsingDutchNationalFlagAlgo(int[] nums) {
+        if(nums.length <= 1) {
+            return;
+        }
+
+        int zeroPointer = 0;
+        int onePointer = 0;
+        int twoPointer = nums.length-1;
+
+        while(onePointer < twoPointer) {
+            if(nums[onePointer] == 0) {
+                swap(nums, zeroPointer, onePointer);
+                zeroPointer++;
+                onePointer++;
+            } else if (nums[onePointer] == 1) {
+                onePointer++;
+            }else {
+                swap(nums, onePointer, twoPointer);
+                onePointer++;
+                twoPointer--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{2,0,2,1,1,0};
 
