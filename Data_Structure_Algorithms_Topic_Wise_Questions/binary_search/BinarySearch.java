500523f43962ce1784b116729fbe792ecb45410b
Vitul Gupta <guptavitul@gmail.com>
2025-06-25 12:11:15 +0530

    Updated the approaches for this problem

--- Data_Structure_Algorithms_Topic_Wise_Questions/binary_search/BinarySearch.java
+++ Data_Structure_Algorithms_Topic_Wise_Questions/binary_search/BinarySearch.java
@@ -2,6 +2,20 @@ package Data_Structure_Algorithms_Topic_Wise_Questions.binary_search;
 
 public class BinarySearch {
 
+    /*
+        Approach 1. Linear Search, iterate in the array and check if the element present or not. If yes,
+                        return that element index else return -1.
+                    TC -> O(n)
+                    SC -> O(1)
+
+        Approach 2. Binary Search, we know Array is sorted so we can use Binary search over here. first we need
+                        to find the middle element if the element is lesser than the mid-element, then we can skip
+                        right side of the middle element or else we can skip the left part.
+
+                    TC -> O(log n)
+                    SC -> O(1)
+     */
+
     public static int search(int[] nums, int target) {
         int lo = 0;
         int hi = nums.length-1;
