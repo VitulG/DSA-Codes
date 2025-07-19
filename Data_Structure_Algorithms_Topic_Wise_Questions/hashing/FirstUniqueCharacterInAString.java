7b3cc8055ef6c324e09f7ce1b7794001d0e09d8f
Vitul Gupta <guptavitul@gmail.com>
2025-07-19 15:45:14 +0530

    Implemented first unique character in a string using hashing

---
+++ Data_Structure_Algorithms_Topic_Wise_Questions/hashing/FirstUniqueCharacterInAString.java
@@ -0,0 +1,30 @@
+package Data_Structure_Algorithms_Topic_Wise_Questions.hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstUniqueCharacterInAString {
+
+    public static int firstUniqChar(String s) {
+        if(s.isEmpty()) {
+            return -1;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+        }
+
+        for(int i=0; i<s.length(); i++) {
+            if(map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("loveleetcode"));
+    }
+}
